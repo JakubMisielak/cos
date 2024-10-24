296c8e7ca26bc8285485f7bef498bd6afb001e6f
bambo <kubamisielak@wp.pl>
2024-10-24 09:34:00 +0200

    porownywanie liczb

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿class Klasa
+{
+    static void Main(string[] args)
+    {
+        int a, b;
+    liczba1:
+        Console.WriteLine("podaj 1 liczbe :");
+        bool mlem = int.TryParse(Console.ReadLine(), out a);
+
+        Console.WriteLine("podaj 2 liczbe do powrownania: ");
+        bool mlen = int.TryParse(Console.ReadLine(), out b);
+        while (mlem == false || mlen == false) 
+        {
+            Console.WriteLine("podales liczbe nie calkowita");
+            goto liczba1;
+             
+        }
+        if (a == b)
+        {
+            Console.WriteLine("liczba jest rowna");
+        }
+        else if (a > b)
+        {
+            Console.WriteLine($"liczba {a} jest wieksza");
+        }
+        else
+        {
+            Console.WriteLine($"liczba {b} jest wieksza");
+        }
+    }
+}
